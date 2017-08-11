47cc8d65e4a0697437228f178217cf4a9ab78ecf
qinchy <qinchy@msn.com>
2017-08-11 18:02:34 +0800

    添加代码

--- src/main/java/com/qinchy/wechat4jdemo/controller/Wechat4jController.java
+++ src/main/java/com/qinchy/wechat4jdemo/controller/Wechat4jController.java
@@ -4,6 +4,7 @@ import com.qinchy.wechat4jdemo.service.impl.Wechat4jService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +17,18 @@ public class Wechat4jController {
 
     Logger log = LoggerFactory.getLogger(Wechat4jController.class);
 
+    @RequestMapping(method = RequestMethod.GET)
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        Wechat4jService service = new Wechat4jService(request);
+        String result = service.execute();
+        try {
+            response.getOutputStream().write(result.getBytes());
+        } catch (IOException e) {
+            log.error("消息处理出错:", e);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
         Wechat4jService service = new Wechat4jService(request);
         String result = service.execute();
